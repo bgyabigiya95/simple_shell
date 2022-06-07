4caa7bc81053ded6f43e0f256348e316d8f4dbbc
Abigiya Haile <bgyabigiya95@gmail.com>
2022-06-07 04:25:45 -0700

    commit

--- f_command_handlers.c
+++ f_command_handlers.c
@@ -61,3 +61,76 @@ int count_args(char *str_input, char *delimiter)
 		tkn_ptr = NULL;
 	}
 
+	free(str_copy);
+
+	return (count);
+}
+
+/**
+ * handle_PATH - Checks if the command to execute could be found in PATH's dirs
+ * @commands: Array of strings containing the command and args
+ * Return: flag 0 if is succes; -1 if is error
+ */
+int handle_PATH(char **commands)
+{
+	char *path_dirs, *path;
+	char *tkn, *tkn_ptr;
+	char *str_copy;
+	int flag = 127;
+
+	if (
+			commands == NULL || commands[0] == NULL ||
+			commands[0][0] == '\0' || commands[0][0] == '/'
+	   )
+		return (127);
+
+	if (access(commands[0], F_OK) == 0)
+		return (0);
+
+	path_dirs = _getenv("PATH");
+	if (path_dirs == NULL)
+		return (127);
+
+	str_copy = duplicate_string(path_dirs);
+	tkn_ptr = str_copy;
+	while (1)
+	{
+		tkn = _strtok(tkn_ptr, ":");
+		if (tkn == NULL)
+			break;
+		tkn_ptr = NULL;
+		path = getpath(tkn, commands[0]);
+		if (access(path, F_OK) != -1)
+		{
+			free(commands[0]);
+			commands[0] = path;
+			flag = 0;
+			break;
+		}
+		free(path);
+	}
+	free(str_copy);
+	return (flag);
+}
+
+/**
+ * getpath - Creates a string representing a full path to file
+ * @dir: String representing a directory path
+ * @filename: Name of the file we are looking for
+ *
+ * Return: String as a full path to "filename"
+ */
+char *getpath(char *dir, char *filename)
+{
+	int dir_len = _strlen(dir);
+	int filename_len = _strlen(filename);
+	char *path;
+
+	path = allocate_memory(sizeof(char *) * (dir_len + filename_len + 2));
+
+	_strcpy(path, dir);
+	_strcat(path, "/");
+	_strncat(path, filename, filename_len + 1);
+
+	return (path);
+}
