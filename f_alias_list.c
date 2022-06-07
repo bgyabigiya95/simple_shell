e442c751905df5d87e5116e3ef7f43ba4772429b
Abigiya Haile <bgyabigiya95@gmail.com>
2022-06-07 03:38:22 -0700

    commit

--- f_alias_list.c
+++ f_alias_list.c
@@ -81,4 +81,86 @@ void set_alias(char *alias_pair)
 	add_node_end(alias_addrs, tmp_buff);
 }
 
+/**
+ * handle_alias_args - Evaluates alias input command and decides what to do
+ * @commands: Arrays of commands
+ * @out_addrs: Pointer to out's head node
+ *
+ * Return: -1 if error happens, 0 otherwise
+ */
+int handle_alias_args(char **commands, list_t **out_addrs)
+{
+	int i, len, was_alias;
+	int status = 0;
+	list_t *curr;
+	list_t **alias_addrs = get_alias_head();
 
+	set_process_exit_code(0);
+	for (i = 1; commands[i] != NULL; i++)
+	{
+		was_alias = 0;
+		len = _strlen(commands[i]);
+		/* Check that if user is trying to print an alias */
+		for (curr = *alias_addrs; curr != NULL; curr = curr->next)
+		{
+			if (_strncmp(curr->str, commands[i], len) == 0 && (curr->str)[len] == '=')
+			{ /* Means to list an alias */
+				was_alias = 1;
+				add_node_end(out_addrs, curr->str);
+				break;
+			}
+		}
+		if (was_alias)
+			continue;
+		/* Here, user is trying to set an alias or print an unexistent one */
+		if (is_set_alias(commands[i]))
+			set_alias(commands[i]); /* Validate that the set format is fine */
+		else /* Print errors */
+		{
+			status = -1;
+			set_process_exit_code(1);
+			_puts("alias: ");
+			_puts(commands[i]);
+			_puts(" not found\n");
+		}
+	}
+
+	return (status);
+}
+
+/**
+ * handle_aliases - Handle alias replacement
+ * @commands: Arrays of commands
+ */
+void handle_aliases(char **commands)
+{
+	list_t *curr;
+	list_t **alias_addrs = get_alias_head();
+	int cmd_len = _strlen(commands[0]);
+	char *str;
+	char tmp_buff[250];
+	int i, alias_len = 0;
+
+	if (commands[0] == NULL)
+		return;
+
+	/* Initialize buffer */
+	for (i = 0; i < 250; i++)
+	tmp_buff[i] = '\0';
+
+	/* Search if a command is an alias */
+	for (curr = *alias_addrs; curr != NULL; curr = curr->next)
+	{
+		str = curr->str;
+		if (_strncmp(commands[0], str, cmd_len) == 0 && str[cmd_len] == '=')
+		{ /* the command is an alias */
+			alias_len = _strlen(&str[cmd_len + 2]); /* +2 beacause of "='" chars */
+			strncpy(tmp_buff, &str[cmd_len + 2], alias_len - 1);
+			tmp_buff[alias_len] = '\0';
+			/* Free and then update the command */
+			free(commands[0]);
+			commands[0] = duplicate_string(tmp_buff);
+			break;
+		}
+	}
+}
